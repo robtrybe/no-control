d5fc9c330013f4113ce560abb34b988fb548db84
Thomas Edson <andepipous@gmail.com>
2025-07-22 17:12:27 -0400

    config: configuração do swagger e aplicação dos ValidationPipe global

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AddressesModule } from './modules/addresses/addresses.module';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [AddressesModule, UsersModule, ConfigModule.forRoot({
+    isGlobal: true,
+    
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+  .setTitle("Documentação No-Control")
+  .addServer(`${process.env.HOST}:${process.env.PORT}`)
+  .addServer(process.env.NGROCK_URL)
+  .setVersion("1.0")
+  // .addBearerAuth() 
+  .build()
+  
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+  
+  const document = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
+  
+
+  await app.listen(process.env.PORT ?? 8080);
+  
 }
 bootstrap();
